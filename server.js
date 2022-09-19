467b51659d86c13999977f79e4b7edc7fee73e40
mrminhb <novafalcongamers@gmail.com>
2022-09-19 09:55:31 +0700

    Implement handlers

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./src/app')
 const PORT = process.env.PORT 
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT} ...`);
+    console.log(`Server is running on port ${PORT} ...`);
 })
--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,13 +1,62 @@
-exports.create = (req, res) => {
-    return res.send({message: 'create handler'});
+const ContactService = require("../services/contact.service");
+const ApiError = require('../api-error');
+
+//create new contact
+exports.create = async (req, res, next) => {
+    if(!req.body?.name) {
+        return next(new ApiError(400, 'Name cannot be empty'))
+    }
+
+    try {
+        const contactService = new ContactService();
+        const contact = await contactService.create(req.body);
+        return res.send(contact);
+
+    } catch(err) {
+        console.log(err);
+        return next(
+            new ApiError(500, 'An error occured while creating the contact')
+        )
+    }
 }
 
-exports.findALl = (req, res) => {
-    return res.send({message: 'findAll handler'});
+//retrive all contacts of a user from the database
+exports.findALl = async (req, res, next) => {
+    let contacts = [];
+
+    try {
+        const contactService = new ContactService();
+        const { name } =  req.query;
+
+        if (name) {
+            contacts = await contactService.findByName(name)
+        } else {
+            contacts = await contactService.all()
+        }
+
+    } catch(err) {
+        console.log(err);
+        return next(
+            new ApiError(500, 'An error occured while creating the contact')
+        )
+    }
+
+    return res.send(contacts)
 }
 
-exports.findOne = (req, res) => {
-    return res.send({message: 'findOne handler'});
+exports.findOne = async (req, res, next) => {
+    try {
+        const contactService = new ContactService();
+        const contact = await contactService.findById(req.params.id);
+        if (!contact) {
+            return next(new ApiError(404, 'Contact not found'))
+        }
+    } catch(err) {
+        console.log(err);
+        return next(
+            new ApiError(500, 'An error occured while creating the contact')
+        )
+    }
 }
 
 exports.update = (req, res) => {
